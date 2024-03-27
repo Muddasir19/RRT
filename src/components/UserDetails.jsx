530525d7156c45b68f17ea722ac3f3b06bc77dd9
Muddasir19 <muddasirahmad19@gmail.com>
2024-03-27 21:27:32 +0500

    update 6 add fake data and use dispatch

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,8 +1,18 @@
 //rafce
 import React from 'react'
 import DeleteAllUser from './DeleteAllUser';
+import { fakeUserData } from '../api';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../store/slices/UserSlice';
 
 const UserDetails = () => {
+
+    const dispatch = useDispatch();
+
+    const addNewUser = (payload) => {
+        dispatch(addUser(payload));
+    };
+
   return (
     <>
         <div className="content">
@@ -10,7 +20,7 @@ const UserDetails = () => {
                 <div className="admin subtitle">
                     List of User Detail
                 </div>
-                <button>Add New Users</button>
+                <button onClick={ ()=> addNewUser(fakeUserData()) } >Add New Users</button>
             </div>
             <ul>
                 <li>hi</li>
