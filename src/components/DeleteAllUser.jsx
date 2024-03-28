51aa5102a8d099d83c439de73f00a42f81e4be8d
Muddasir19 <muddasirahmad19@gmail.com>
2024-03-28 22:08:31 +0500

    Update 9 Delete All Users

--- src/components/DeleteAllUser.jsx
+++ src/components/DeleteAllUser.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteUser } from '../store/slices/UserSlice';
 
 const DeleteAllUser = () => {
+  const dispatch = useDispatch();
+  const deleteAll = () => {
+    dispatch(deleteUser())
+  }
   return (
     <>
     <div>Delete All Users</div>
+    <button onClick={()=> deleteAll()} >Delete All Users</button>
       
     </>
   )
--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -11,11 +11,13 @@ const userSlice  = createSlice({
         removeUser(state,action){
             state.splice(action.payload,1)
         },
-        deleteUser(state,action){},
+        deleteUser(state,action){
+            return [];
+        },
     }
 });
 
 console.log(userSlice)
 
 export default  userSlice.reducer;
-export const {addUser,removeUser} = userSlice.actions;
+export const {addUser,removeUser,deleteUser} = userSlice.actions;
