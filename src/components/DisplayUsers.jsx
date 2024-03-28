d3c6f6f40681a48af4029cef1799121fdedfb929
Muddasir19 <muddasirahmad19@gmail.com>
2024-03-28 22:03:53 +0500

    update 8 delete single user

--- src/components/DisplayUsers.jsx
+++ src/components/DisplayUsers.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
+import { removeUser } from '../store/slices/UserSlice';
+// import {MdDeleteForever} from "react-icons"
 
 const DisplayUsers = () => {
+    const dispatch = useDispatch();
     const data = useSelector((state) => {
         return state.users;
     })
+    const deleteUser = (id) => {
+        dispatch(removeUser(id))
+    }
   return (
     <div>
         {
             data.map((user,id) => {
                 return <li key={id}>
                     {user}
+                    <button className='btn-delete' onClick={()=> deleteUser(id)} > Remove
+                        {/* <MdDeleteForever className="delete-icon"/> */}
+                    </button>
                 </li>
             })
         }
